7938540b57cc7691b7bf035af4152e0f5f3ccb97
scalabrese <simoncalabrese94@gmail.com>
2020-12-12 15:19:51 +0100

    no records now

--- report/__init__.py
+++ report/__init__.py
@@ -2,11 +2,13 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+import glob
+import os
+
 import scrapy
 import re
-
 from scrapy import Selector
-from scrapy.crawler import CrawlerProcess, CrawlerRunner
+from scrapy.crawler import CrawlerRunner
 from scrapy.spiders import CrawlSpider, Rule, Spider
 from scrapy.linkextractors import LinkExtractor
 from twisted.internet import reactor, defer
@@ -14,70 +16,64 @@ from scrapy.utils.log import configure_logging
 import pandas as pd
 
 
-def get_href(record):
-    first = re.sub("(a href=\"#)([a-z]*)(\")", "", record)
-    print(first)
+class ReportCrawler:
+    author_urls = []
+    records = dict()
+
+    class AllAuthorsCrawler(CrawlSpider):
+        name = 'AllAuthors'
+
+        rules = (Rule(LinkExtractor(allow=('/author/')), callback='cache_author_links'),)
+        start_urls = ['https://blog.griddynamics.com/all-authors/']
+
+        def cache_author_links(self, response):
+            ReportCrawler.author_urls.append(response._get_url())
+
+    class AllArticlesByAuthorCrawler(Spider):
+        name = 'AllArticlesByAuthor'
+
+        def start_requests(self):
+            for one_url in ReportCrawler.author_urls:
+                yield scrapy.Request(url=one_url, callback=self.print)
+
+        def print(self, response):
+            key_url = response._get_url().rsplit('/')[-2] #get the author nickname after the last slash
+            name = response.xpath('//*[@id="woe"]/div[2]/div/div[1]/div[2]/h3/text()').extract_first()
+            date_title_part = response.xpath('//*[@id="woe"]/div[2]/div/div[2]/div[position() > 1]')
+            articles = []
+            for row in date_title_part:
+                date = Selector(text=row.extract()).xpath('///span/text()').extract_first()
+                title = Selector(text=row.extract()).xpath('///a/text()').extract_first()
+                articles.append({'name': name, 'date': date, 'title': title})
+            author_articles = {'authorKey': key_url, 'articles': articles}
+            ReportCrawler.records.update(author_articles)
 
 
-class AllAuthorsCrawler(CrawlSpider):
-    name = 'AllAuthors'
+            # df = pd.DataFrame(eval(str(self.records)))
+            # df.to_json(f"{key_url}-data.json")
 
-    rules = (Rule(LinkExtractor(allow=('/author/')), callback='cache_author_links'),)
-    start_urls = ['https://blog.griddynamics.com/all-authors/']
-
-    def cache_author_links(self, response):
-        with open('authors.txt', 'a+') as f:
-            f.write(response._get_url() + '\n')
-
-
-class AllArticlesByAuthorCrawler(Spider):
-    name = 'AllArticlesByAuthor'
-    records = []
-
-    def fetch_urls(self):
-        author_urls = []
-        with open('authors.txt') as f:
-            for line in f:
-                author_urls.append(line)
-        return author_urls
-
-    def start_requests(self):
-        urls = self.fetch_urls()
-        for one_url in urls:
-            yield scrapy.Request(url=one_url, callback=self.print)
-
-    def print(self, response):
-        name = response.xpath('//*[@id="woe"]/div[2]/div/div[1]/div[2]/h3/text()').extract_first()
-        date_title_part = response.xpath('//*[@id="woe"]/div[2]/div/div[2]/div[position() > 1]')
-        for row in date_title_part:
-            date = Selector(text=row.extract()).xpath('///span/text()').extract_first()
-            title = Selector(text=row.extract()).xpath('///a/text()').extract_first()
-            record = {'name': name, 'date': date, 'title': title}
-            self.records.append(record)
-
+    def save_csv(self):
         df = pd.DataFrame(eval(str(self.records)))
-        df.to_csv('authors_data.csv', mode='a', header=False)
+        df.to_csv(f"data.csv")
+        #does not work
+
+    def crawl(self):
+        configure_logging()
+        runner = CrawlerRunner()
+
+        @defer.inlineCallbacks
+        def crawl():
+            yield runner.crawl(ReportCrawler.AllAuthorsCrawler)
+            yield runner.crawl(ReportCrawler.AllArticlesByAuthorCrawler)
+            reactor.stop()
+
+        crawl()
+        reactor.run()  # the script will block here until the last crawl call is finished
 
 
 if __name__ == "__main__":
-    # process = CrawlerProcess()
-    # process.crawl(AllAuthorsCrawler)
-    # process.start()
-    #
-    # process.crawl(AllArticlesByAuthorCrawler)
-    # process.start()
-    configure_logging()
-    runner = CrawlerRunner()
-
-
-    @defer.inlineCallbacks
-    def crawl():
-        yield runner.crawl(AllAuthorsCrawler)
-        yield runner.crawl(AllArticlesByAuthorCrawler)
-        reactor.stop()
-
-
-    crawl()
-    reactor.run()  # the script will block here until the last crawl call is finished
+    crawler = ReportCrawler()
+    crawler.crawl()
+    crawler.save_csv()
 
     # create another process that calls several Spiders for each pourpose
