880665a799ad501856b75e2403c924939514420a
scalabrese <simoncalabrese94@gmail.com>
2020-12-14 11:31:40 +0100

    wrote in folder data

--- report/__init__.py
+++ report/__init__.py
@@ -2,23 +2,24 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
-import glob
-import os
-
 import scrapy
-import re
 from scrapy import Selector
 from scrapy.crawler import CrawlerRunner
 from scrapy.spiders import CrawlSpider, Rule, Spider
 from scrapy.linkextractors import LinkExtractor
 from twisted.internet import reactor, defer
 from scrapy.utils.log import configure_logging
-import pandas as pd
 
 
 class ReportCrawler:
     author_urls = []
-    records = dict()
+
+    def __init__(self) -> None:
+        import os
+        if os.path.exists(os.path.dirname("./data/")):
+            import shutil
+            shutil.rmtree(os.path.dirname("./data/"))
+        os.makedirs(os.path.dirname("./data/"))
 
     class AllAuthorsCrawler(CrawlSpider):
         name = 'AllAuthors'
@@ -30,13 +31,15 @@ class ReportCrawler:
             ReportCrawler.author_urls.append(response._get_url())
 
     class AllArticlesByAuthorCrawler(Spider):
+
         name = 'AllArticlesByAuthor'
 
         def start_requests(self):
             for one_url in ReportCrawler.author_urls:
-                yield scrapy.Request(url=one_url, callback=self.print)
+                # yield scrapy.Request(url=one_url, callback=self.parse)
+                yield scrapy.Request(url=one_url)
 
-        def print(self, response):
+        def parse(self, response, **kwargs):
             key_url = response._get_url().rsplit('/')[-2] #get the author nickname after the last slash
             name = response.xpath('//*[@id="woe"]/div[2]/div/div[1]/div[2]/h3/text()').extract_first()
             date_title_part = response.xpath('//*[@id="woe"]/div[2]/div/div[2]/div[position() > 1]')
@@ -45,17 +48,9 @@ class ReportCrawler:
                 date = Selector(text=row.extract()).xpath('///span/text()').extract_first()
                 title = Selector(text=row.extract()).xpath('///a/text()').extract_first()
                 articles.append({'name': name, 'date': date, 'title': title})
-            author_articles = {'authorKey': key_url, 'articles': articles}
-            ReportCrawler.records.update(author_articles)
 
-
-            # df = pd.DataFrame(eval(str(self.records)))
-            # df.to_json(f"{key_url}-data.json")
-
-    def save_csv(self):
-        df = pd.DataFrame(eval(str(self.records)))
-        df.to_csv(f"data.csv")
-        #does not work
+            with open(f"./data/{key_url}-data.json","w") as f:
+                f.write(str(articles))
 
     def crawl(self):
         configure_logging()
@@ -74,6 +69,7 @@ class ReportCrawler:
 if __name__ == "__main__":
     crawler = ReportCrawler()
     crawler.crawl()
-    crawler.save_csv()
+
+    # crawler.save_csv()
 
     # create another process that calls several Spiders for each pourpose
