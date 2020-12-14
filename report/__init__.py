07a0dfb94ece7872585d28de7d6e66006f12d231
FLeuci <fleuci@griddynamics.com>
2020-12-14 16:50:46 +0100

    data_format

--- report/__init__.py
+++ report/__init__.py
@@ -4,7 +4,10 @@
 # your spiders.
 import scrapy
 import re
+import json
+from datetime import datetime
 
+from pandas._libs.tslibs import strptime
 from scrapy import Selector
 from scrapy.crawler import CrawlerProcess
 from scrapy.spiders import CrawlSpider, Rule
@@ -12,43 +15,36 @@ from scrapy.linkextractors import LinkExtractor
 
 
 def get_href(record):
-    first = re.sub("(a href=\"#)([a-z]*)(\")", "",record)
+    first = re.sub("(a href=\"#)([a-z]*)(\")", "", record)
     print(first)
 
 
 class WebCrawler(CrawlSpider):
     name = 'Start'
 
-    #def start_requests(self):
-    #    urls = 'https://blog.griddynamics.com/'
-    #   yield scrapy.Request(url=urls, callback=self.parse)
-
-    rules = (Rule(LinkExtractor(allow = ('/author/')), callback = 'parse1'),)
+    rules = (Rule(LinkExtractor(allow='/author/'), callback='parse1'),)
     start_urls = ['https://blog.griddynamics.com/all-authors/']
 
-    # def start_requests(self):
-    #     urls2 = 'https://blog.griddynamics.com/all-authors/'
-    #     yield scrapy.Request(url=urls2, callback=self.parse2)
     def parse1(self, response):
-
         author_name = response.xpath('//*[@id="woe"]/div[2]/div/div[1]/div[2]/h3/text()').extract_first()
-
         date_title_part = response.xpath('//*[@id="woe"]/div[2]/div/div[2]/div[position() > 1]')
         job_title = response.xpath('//*[@id="woe"]/div[2]/div/div[1]/div[2]/p/text()').extract_first()
         linkedin_url = response.xpath('//*[@id="woe"]/div[2]/div/div[1]/div[1]/ul/li/a/@href').extract_first()
         for row in date_title_part:
             date = Selector(text=row.extract()).xpath('///span/text()').extract_first()
+            converted_date = datetime.strptime(date, '%B %d, %Y')
             title = Selector(text=row.extract()).xpath('///a/text()').extract_first()
-            print(f"name: {author_name}, date: {date} \t \t title: {title}, job_title: {job_title}, linkedin_url: {linkedin_url}")
-            with open('authors.json', 'a+') as f:
-                f.write(f"author: {author_name}, date: {date} \t \t title: {title}, job title: {job_title}, linkedin url: {linkedin_url}, '\n',")
-
-
-
-
+            print(f"name: {author_name}, date: {converted_date} \t \t title: {title}, job_title: {job_title}, linkedin_url: {linkedin_url}")
+            with open('results.txt', 'a+') as outfile:
+                outfile.write(f"name: {author_name} \t,"
+                              f" date: {converted_date} \t \t title: {title} \t,"
+                              f" job_title: {job_title} \t,"
+                              f" linkedin_url: {linkedin_url} \n")
+                #json.dump(results, outfile)
+                #f.write(f"author: {author_name}, date: {date} \t \t title: {title}, job title: {job_title}, linkedin url: {linkedin_url}, '\n',")
 
     def get_href(private, record):
-        first = re.sub(r"(<a href=\"#)","",record)
+        first = re.sub(r"(<a href=\"#)", "", record)
         print(first)
 
 
@@ -62,32 +58,6 @@ class WebCrawler(CrawlSpider):
 
     def clean_from_tag(self, tag):
         return re.sub(r"[\n\t]*", '', re.sub(r"<.*?>", '', tag))
-
-#        author_list = response.xpath('//*[@id="woe"]/section/div/div[2]/a/article/div[2]/span/strong').extract()
- #       author_list_clean = map(self.clean_from_tag, author_list)
-  #      list_author = list(author_list_clean)
-   #     print('\n'.join(list_author))
-#
- #       substring = 'a'
-  #      count = list_author.count(substring)
-   #     print(count)
-#
-#        date_list = response.xpath('//*[@id="woe"]/section/div/div[2]/a/article/div[2]/span/text()').extract()
-#        date_list_clean = map(self.clean_from_tag, date_list)
-#        print('\n'.join(date_list_clean))
-#
-
-
-    #    section = response.xpath('//*[@id="woe"]/section/div/div[2]/a/article')
-     #   print(section)
-      #  sec = section.xpath('div[2]/span/strong')
-       # print(sec)
-
-        #yield {
-         #   'author': author_list_clean,
-          #  'article': article_list_clean,
-           # 'date': date_list_clean
-        #}
 
 
 if __name__ == "__main__":
