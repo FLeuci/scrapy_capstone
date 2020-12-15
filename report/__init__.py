df1c4541990e6c8d5e631d5bd25abdb2bb6ccbde
FLeuci <fleuci@griddynamics.com>
2020-12-15 16:12:35 +0100

    data_format

--- report/__init__.py
+++ report/__init__.py
@@ -10,59 +10,122 @@ from datetime import datetime
 from pandas._libs.tslibs import strptime
 from scrapy import Selector
 from scrapy.crawler import CrawlerProcess
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import CrawlSpider, Rule, Spider
 from scrapy.linkextractors import LinkExtractor
 
+# if __name__ == "__main__":
+#    process = CrawlerProcess()
+#    process.crawl(WebCrawler)
+#    process.start()
 
-def get_href(record):
-    first = re.sub("(a href=\"#)([a-z]*)(\")", "", record)
-    print(first)
+# create another process that calls several Spiders for each pourpose
+from scrapy import Selector
+from scrapy.crawler import CrawlerRunner
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.utils.log import configure_logging
+from twisted.internet import reactor, defer
+import json
+#import report.Configs as conf
+from datetime import datetime
 
 
-class WebCrawler(CrawlSpider):
-    name = 'Start'
+class ReportCrawler:
+    articles = []
+    articles_details = []
 
-    rules = (Rule(LinkExtractor(allow='/author/'), callback='parse1'),)
-    start_urls = ['https://blog.griddynamics.com/all-authors/']
+    def __init__(self):
+        import os
+        if os.path.exists(os.path.dirname("./data/")):
+            import shutil
+            shutil.rmtree(os.path.dirname("./data/"))
+        os.makedirs(os.path.dirname("./data/"))
 
-    def parse1(self, response):
-        author_name = response.xpath('//*[@id="woe"]/div[2]/div/div[1]/div[2]/h3/text()').extract_first()
-        date_title_part = response.xpath('//*[@id="woe"]/div[2]/div/div[2]/div[position() > 1]')
+    def crawl(self):
+        configure_logging()
+        runner = CrawlerRunner()
+
+        @defer.inlineCallbacks
+        def crawl():
+            yield runner.crawl(AuthorInfoCrawler)
+            reactor.stop()
+
+        crawl()
+        reactor.run()  # the script will block here until the last crawl call is finished
+
+    def get_base_url(self):
+        self.base_url = 'https://blog.griddynamics.com/'
+        return self.base_url
+
+    def export_json(self):
+        with open('./data/data.json', 'w') as f:
+            f.write(str(self.articles))
+
+
+class AuthorInfoCrawler(CrawlSpider):
+    name = 'AuthorInfoCrawler'
+    rules = (Rule(LinkExtractor(allow=('/author/')), callback='parse'),)
+    start_urls = ['https://blog.griddynamics.com//all-authors/']
+
+    def parse(self, response, **kwargs):
+        key_url = response._get_url().rsplit('/')[-2]  # get the author nickname after the last slash
+        name = response.xpath('//*[@id="woe"]/div[2]/div/div[1]/div[2]/h3/text()').extract_first()
         job_title = response.xpath('//*[@id="woe"]/div[2]/div/div[1]/div[2]/p/text()').extract_first()
         linkedin_url = response.xpath('//*[@id="woe"]/div[2]/div/div[1]/div[1]/ul/li/a/@href').extract_first()
+        date_title_part = response.xpath('//*[@id="woe"]/div[2]/div/div[2]/div[position() > 1]')
         for row in date_title_part:
-            date = Selector(text=row.extract()).xpath('///span/text()').extract_first()
-            converted_date = datetime.strptime(date, '%B %d, %Y')
-            title = Selector(text=row.extract()).xpath('///a/text()').extract_first()
-            print(f"name: {author_name}, date: {converted_date} \t \t title: {title}, job_title: {job_title}, linkedin_url: {linkedin_url}")
-            with open('results.txt', 'a+') as outfile:
-                outfile.write(f"name: {author_name} \t,"
-                              f" date: {converted_date} \t \t title: {title} \t,"
-                              f" job_title: {job_title} \t,"
-                              f" linkedin_url: {linkedin_url} \n")
-                #json.dump(results, outfile)
-                #f.write(f"author: {author_name}, date: {date} \t \t title: {title}, job title: {job_title}, linkedin url: {linkedin_url}, '\n',")
-
-    def get_href(private, record):
-        first = re.sub(r"(<a href=\"#)", "", record)
-        print(first)
+            row_extracted = row.extract()
+            raw_date = Selector(text=row_extracted).xpath('///span/text()').extract_first()
+            date = datetime.strptime(raw_date, '%B %d, %Y').strftime('%Y%m%d')
+            article_title = Selector(text=row_extracted).xpath('///a/text()').extract_first()
+            article_url = Selector(text=row_extracted).xpath('///a/@href').extract_first()
+            ReportCrawler.articles.append({'keyUrl': key_url,
+                                           'name': name,
+                                           'jobTitle': job_title,
+                                           'linkedinUrl': linkedin_url,
+                                           'date': date,
+                                           'article_title': article_title,
+                                           'article_url': article_url})
 
 
-    def parse2(self, response):
-        autori = list(response.xpath('//*[@id="authorsmini"]/div/a').getall())
-        autori_list_clean = map(self.clean_from_tag, autori)
-        list_autori = list(autori_list_clean)
-        print('\n'.join(list_autori))
-        print(len(list_autori))
+class ArticleInfoCrawler(Spider):
+    name = 'ArticleInfoCrawler'
 
 
-    def clean_from_tag(self, tag):
-        return re.sub(r"[\n\t]*", '', re.sub(r"<.*?>", '', tag))
+    def start_requests(self):
+        author_link_list = list(
+            map(lambda obj: (obj['keyUrl'], 'https://blog.griddynamics.com' + obj['article_url']),
+                ReportCrawler.articles))
+        for link in author_link_list:
+            yield scrapy.Request(url= link[1])
+        return super().start_requests()
+        #yield_url
+
+
+    def parse(self, response, **kwargs):
+
+        title = response.xpath('//*[@id="wrap"]/h1/text()').extract_first()
+        url_to_full_version = response._get_url()
+        body = response.path('//*[@id="woe"]/section/div/p/text()').extract()
+        first_160 = body[:160]
+        publication_date = response.xpath('//*[@id="wrap"]/div/div[2]/text()').extract_first()
+        authors_fullname = response.xpath('//*[@id="wrap"]/div/div[position() > 1]')
+        tags = response.xpath('//*[@id="woe"]/section/div/div/text()').extract()
+        for row in authors_fullname:
+            row_extracted = row.extract()
+            author_fullname = Selector(text=row_extracted).xpath('///span/text()').extract_first()
+            key_url = Selector(text=row_extracted).xpath('///a/@href').extract_first()
+            ReportCrawler.articles_details.append({'title': title,
+                                                   'url': url_to_full_version,
+                                                   'first 160': first_160,
+                                                   'date': publication_date,
+                                                   'author': author_fullname,
+                                                   'article_url': key_url,
+                                                   'tag': tags })
+
 
 
 if __name__ == "__main__":
-    process = CrawlerProcess()
-    process.crawl(WebCrawler)
-    process.start()
-
-    #create another process that calls several Spiders for each pourpose
+    crawler = ReportCrawler()
+    crawler.crawl()
+    crawler.export_json()
