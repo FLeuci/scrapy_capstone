e73ca28179b8607f7832682ab7f250e0b6ae0680
FLeuci <fleuci@griddynamics.com>
2020-12-28 11:43:38 +0100

    update

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-#
+#Parse GridDynamics Blog with Scrapy and visualize it
+## Introduction
+   In this project a parser will be created for GridDynamics blog together with a report that will show us:
+      
+    - Top-5 Authors,
+    - Top-5 New Articles,
+    - Plot with articles counter of 7 most popular tags
+## Technologies
+    - Python 3.9
+    - Scrapy
+    - matplotlib
+    - pandas
+    - os
+    - JSON
+## Features
+The aim of this project is to create a Web Crawler that will parse https://blog.griddynamics.com/,
+obtain articles and author information from it.
+Then generate a report with this information:
+
+    - Top-5 Authors (based on articles counter)
+    - Top-5 New Articles (based on publish data)
+    - Plot with counts of 7 popular tags
+## Sources 
+    https://app.pluralsight.com/library/courses/scrapy-extracting-structured-data/table-of-contents
+    https://app.pluralsight.com/library/courses/data-visualization-with-python-introduction/table-of-contents
+    https://app.pluralsight.com/library/courses/python-fundamentals/table-of-contents
 
 
--- report.py
+++ report.py
@@ -5,42 +5,32 @@ import json
 import collections
 import matplotlib.pyplot as plt
 import logging
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(filename='test.log', level=logging.INFO)
 if __name__ == "__main__":
     if conf.base_path_empty:
         crawler = rc.ReportCrawler()
         crawler.crawl()
         crawler.export_json()
 
-
-    def removeduplicate(it):
-        seen = set()
-        for x in it:
-            t = tuple(x.items())
-            if t not in seen:
-                yield x
-                seen.add(t)
-
-
+    # Reading from Json files
     list_of_authors = pd.read_json('/Users/fleuci/report/data/authors.json')
     list_of_articlesJson = pd.read_json('/Users/fleuci/report/data/articles.json')
     list_of_articles = list_of_articlesJson.drop_duplicates(subset='title')
 
-
-    # TOP 5 AUTHORS
+    # Getting a list of top 5 authors by number of articles
     authorsCounter = list_of_authors.groupby("name")["article_title"].count()
     top5_authors = authorsCounter.sort_values(ascending=False)
+    logging.info('List of 5 best author for number of written articles: {}'.format(top5_authors[:5]))
     print(top5_authors[:5])
 
-    # TOP 5 NEW ARTICLES
+    # Getting a list of top 5 new articles
     top5_articles = pd.DataFrame(list_of_articles, columns=['title', 'dateFormatted'])
     top5_articles.sort_values(by=['dateFormatted'], inplace=True, ascending=False)
+    logging.info('List of 5 most recent articles: {}'.format(top5_articles[:5]))
     print(top5_articles[:5])
 
-    # Plot with counts of 7 popular tags:
-    # it must be a bar chart (column plot) where each column is for one tag
-    # Tag bar must have a name in the plot.
-    # X-axis - counter with articles of tag theme
+    # Plot with counts of 7 popular tags
     all_tags = list(list_of_articles.loc[:, "tags"])
     count = []
     for tag in all_tags:
@@ -54,7 +44,6 @@ if __name__ == "__main__":
         tags = {'tag': key, 'quantity': value}
         keys.append(key)
         values.append(value)
-
         with open(f"{conf.base_path}tags.json", "a") as f:
             f.write(json.dumps(tags))
     graph = plt.bar(keys[:7], values[:7])
@@ -62,13 +51,5 @@ if __name__ == "__main__":
     plt.xlabel('Tags')
     plt.ylabel("Tag's quantity")
     plt.title('Top 7 most popular tags')
-    plt.show()
-
-    # list_of_tags = pd.read_json('/Users/fleuci/main/data/tags.json')
-    # print(list_of_tags)
-
-    # articles_authors = list_of_articles.merge(list_of_authors, left_on='author_key', right_on='keyUrl')
-    # best_authors = articles_authors.groupby('author_key')['urlFullVersion'].count().sort_values(ascending=False)
-    # print(best_authors[:5])
-
-# do main
+    plot = plt.show()
+    logging.info('Bar chart representing the 5 most popular tags')
